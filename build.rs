6fcadc405d8dd481dba0dc6810bed9404b14d670
David Heise <daheise@users.noreply.github.com>
2015-05-23 13:09:06 -0400

    Updated travis and build.rs to use out of course build for spsps

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main(){
     println!("cargo:rustc-link-lib=static=spsps");
-    println!("cargo:rustc-link-search=native=./spsps/");
-    println!("cargo:libdir=./spsps");
+    println!("cargo:rustc-link-search=native=./spsps/build");
+    println!("cargo:libdir=./spsps/build");
 }
