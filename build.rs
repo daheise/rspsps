231e750776910445cb00f4497a3cf0be3623f5cf
David Heise <daheise@users.noreply.github.com>
2015-05-23 20:18:26 -0400

    Updated build.rs to let cargo run anywhere in the directory tree

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
+use std::env;
+
 fn main(){
+    let cargo_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let spps_lib_dir = cargo_dir + "/spsps/build";
     println!("cargo:rustc-link-lib=static=spsps");
-    println!("cargo:rustc-link-search=native=./spsps/build");
+    println!("cargo:rustc-link-search=native={}", spps_lib_dir);
     println!("cargo:libdir=./spsps/build");
 }
