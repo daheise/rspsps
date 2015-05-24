de51f6a3e6a17a1cb7dfd5842ceb1299bb493e77
David Heise <daheise@users.noreply.github.com>
2015-05-24 17:54:06 -0400

    Working on getting documentation up.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! A library the provides a safe Rust wrapper for 
+//! [Stacy's Pathetically Simple Parsing System](https://github.com/sprowell/spsps).
+
 extern crate libc;
 #[macro_use]
 extern crate log;
@@ -15,8 +18,9 @@ use std::ffi::CString;
 
 
 
-
+/// For now we're treating spsps_loc_ as an opaque pointer.
 enum spsps_loc_ {}
+/// An opaque type for spsps_errno. Currently unused.
 enum spsps_errno {}
 
 /*
@@ -27,17 +31,22 @@ struct Loc{
 	column: u32,
 }
 */
-//This is for the opaque C pointer
+
+/// This is for the opaque C pointer to spsps_parser_
 enum spsps_parser_ {}
-//this will be the safe abstraction
+/// This is the safe abstraction over a spsps parser.
 #[derive(Debug)]
-struct Parser{
+pub struct Parser{
+	/// The raw pointer to a C spsps parser.
 	_ptr: *mut spsps_parser_,
+	/// A raw pointer a C file stream. May be null for stdin.
 	_fd: *mut libc::FILE
 }
 
 #[derive(Debug)]
-struct Loc{
+/// A safe abstruction over a raw spsps_loc_ pointer.
+pub struct Loc{
+	/// The *mut spsps_loc_pointer. 
 	ptr: *mut spsps_loc_
 }
 
@@ -65,7 +74,9 @@ extern{
 }
 
 impl Parser{
-	fn new() -> Parser {
+	/// Create a new `Parser` with zero configuration. Name will use the default from libspsps,
+	/// and will read from stdin.
+	pub fn new() -> Parser {
 		unsafe{
 			Parser{ 
 		    	_ptr: spsps_new(ptr::null(), ptr::null_mut()),
@@ -73,7 +84,10 @@ impl Parser{
 			}
 		 }
 	}
-	fn from_file(name: Option<&str>, path: &Path) -> Parser {
+	
+	/// Create a Parser that will read from the file given in `path`. If `None` is given for `name`
+	/// the default spsps parser name will be used.
+	pub fn from_file(name: Option<&str>, path: &Path) -> Parser {
 		unsafe{
 			let c_path = CString::new(path.to_str().unwrap()).unwrap().as_ptr();
 			debug!("from_file path: {:?}", CString::new(path.to_str().unwrap()).unwrap());
@@ -94,36 +108,48 @@ impl Parser{
 		 }
 	}
 	
-	fn consume(&self) -> String {
+	/// Consume and return the next character in the stream.
+	pub fn consume(&self) -> String {
 		unsafe {
 			char::from_u32(spsps_consume(self._ptr) as u32).unwrap().to_string()
 		}
 	}
 	
-	fn consume_n(&self, n: usize){
+	/// Consume and discard the next `n` characters from the stream.
+	pub fn consume_n(&self, n: usize){
 		unsafe { spsps_consume_n(self._ptr, n as libc::size_t); }
 	}
 	
-	fn consume_whitespace(&self){
+	/// Consume and discard all whitespace.  When this method returns the next
+    /// character is the first non-whitespace character, or the end of file has
+    /// been reached.
+	pub fn consume_whitespace(&self){
 		unsafe { spsps_consume_whitespace(self._ptr); }
 	}
 	
-	fn eof(&self) -> bool{
+	///Determine if the end of file has been consumed.
+	pub fn eof(&self) -> bool{
 		unsafe { spsps_eof(self._ptr) as usize != 0 } 
 	}
 	
-	fn get_loc(&self) -> Loc {
+	/// Get the current location in the stream.  This is the location of the next
+    /// character to be read, unless the end of stream has been reached.
+	pub fn get_loc(&self) -> Loc {
 		unsafe{
 			Loc { ptr: spsps_loc(self._ptr) }
 		}
 	}
 	
-	fn peek(&self) -> String{
+	///Peek and return the next character in the stream.  The character is not consumed.
+	pub fn peek(&self) -> String{
 		let c_char = unsafe { spsps_peek(self._ptr) };
 		char::from_u32(c_char as u32).unwrap().to_string()
 	}
 	
-	fn peek_n(&self, n: usize) -> String{
+	/// Peek ahead at the next few characters in the stream, and return them.  The
+    /// The number of characters must be below the lookahead limit.  End of file causes the
+    /// returned string to be populated by EOF characters. 
+	pub fn peek_n(&self, n: usize) -> String{
 		let cstr = unsafe{ spsps_peek_n(self._ptr, n as libc::size_t) };
 		let safe_cstr = unsafe { CStr::from_ptr(cstr) };
 		let retval = str::from_utf8(safe_cstr.to_bytes()).unwrap_or("").to_owned();
@@ -131,11 +157,15 @@ impl Parser{
 		return retval;
 	}
 	
-	fn peek_str(&self, needle: &str) -> bool {
+	/// Peek ahead and determine if the next characters in the stream are the given
+    /// characters, in sequence.  That is, the given string must be the next thing in the stream.
+	pub fn peek_str(&self, needle: &str) -> bool {
 		unsafe { spsps_peek_str(self._ptr, CString::new(needle).unwrap().as_ptr()) != 0 }
 	}
 	
-	fn peek_str_and_consume(&self, needle: &str) -> bool {
+	///Peek ahead at the next few characters and if they are a given string, then
+    /// consume them.  Otherwise leave the stream unchanged.
+	pub fn peek_str_and_consume(&self, needle: &str) -> bool {
 		unsafe { spsps_peek_and_consume(self._ptr, CString::new(needle).unwrap().as_ptr()) != 0 }
 	}
 }
@@ -149,7 +179,8 @@ impl Drop for Parser{
 	}
 }
 
-impl Loc{
+impl ToString for Loc{
+	///Convert this location to a short string.
 	fn to_string(&self) -> String{
 		let tmp = unsafe { CStr::from_ptr(spsps_loc_to_string(self.ptr)) };
 		let retval = str::from_utf8(tmp.to_bytes()).unwrap_or("").to_owned();
@@ -185,9 +216,9 @@ fn test_from_file(){
 #[test]
 fn test_peek_n(){
 	let p = test_parser();
-	let first_chars = p.peek_n(3);
-	println!("{:?}", first_chars);
-	assert_eq!(first_chars, "Thi");
+	let first_chars = p.peek_n(4);
+	println!("{}", first_chars);
+	assert_eq!(first_chars, "This");
 }
 
 #[test]
@@ -202,7 +233,7 @@ fn test_peek(){
 fn test_consume(){
 	let p = test_parser();
 	let first_char = p.consume();
-	println!("{:?}", first_char);
+	println!("{}", first_char);
 	assert_eq!(first_char, "T");
 }
 
